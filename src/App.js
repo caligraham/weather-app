0d274e782b81662d1e0ab68a2fe6c18c2f417c50
Rebecca <graham.cali@gmail.com>
2021-05-12 16:37:06 -0500

    getting current location

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
+import Weather from "./components/weather";
 
 export default function App() {
 
@@ -8,17 +9,30 @@ const [long, setLong] = useState([]);
 const [data, setData] = useState([]);
 
 useEffect(() => {
+  const fetchData = async () => {
   navigator.geolocation.getCurrentPosition(function(position) {
     setLat(position.coords.latitude);
     setLong(position.coords.longitude);
   });
 
-  console.log("Latitude is:", lat)
-  console.log("Longitude is:", long)
+  await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+  .then(res => res.json())
+  .then(result => {
+    setData(result)
+    console.log(result);
+  });
+  }
+
+ fetchData()
 }, [lat, long]);
 
   return (
     <div className="App">
+      {(typeof data.main != 'undefined') ? (
+        <Weather weatherData={data}/>
+      ): (
+        <div></div>
+      )}
    
     </div>
   );
