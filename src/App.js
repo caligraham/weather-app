fe5efbb56cf8097bb68dcdf7577a98cf2f80b6c4
Rebecca <graham.cali@gmail.com>
2021-05-12 16:21:27 -0500

    getting lat & long rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-
+import React, { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
+export default function App() {
+
+const [lat, setLat] = useState([]);
+const [long, setLong] = useState([]);
+const [data, setData] = useState([]);
+
+useEffect(() => {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    setLat(position.coords.latitude);
+    setLong(position.coords.longitude);
+  });
+
+  console.log("Latitude is:", lat)
+  console.log("Longitude is:", long)
+}, [lat, long]);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+   
     </div>
   );
 }
 
-export default App;
